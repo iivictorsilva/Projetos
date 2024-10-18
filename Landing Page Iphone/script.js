cdef8aa4d35e06e4369d75655a7a6f66f10e3303
Victor Augusto <vsilva.a.augusto@gmail.com>
2024-10-17 22:57:05 -0300

    Iphone

---
+++ Landing Page Iphone/script.js
@@ -0,0 +1,10 @@
+function imgSlider(e){
+    document.querySelector('#phone').src = e;
+
+}
+
+
+function circleChange(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
